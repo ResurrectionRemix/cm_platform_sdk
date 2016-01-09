f22159bdff77834d52d9bd167e438518cd0b6ebd
LorDClockaN <davor@losinj.com>
2016-01-09 22:20:09 +0530

    Add SystemUI Restart Tile [2/2]

--- src/java/org/cyanogenmod/internal/util/QSConstants.java
+++ src/java/org/cyanogenmod/internal/util/QSConstants.java
@@ -65,7 +65,8 @@ public class QSConstants {
     public static final String TILE_NAVBAR = "navbar";	
     public static final String TILE_PIE = "pie";	
     public static final String TILE_APPCIRCLEBAR = "appcirclebar";
-    public static final String TILE_APPSIDEBAR = "appsidebar";					
+    public static final String TILE_APPSIDEBAR = "appsidebar";	
+    public static final String TILE_RESTARTUI = "restartui";					
     public static final String TILE_KERNEL= "kernel_adiutor";	
     public static final String TILE_SCREENRECORD= "screenrecord";	
     public static final String DYNAMIC_TILE_NEXT_ALARM = "next_alarm";
@@ -123,6 +124,7 @@ public class QSConstants {
 	STATIC_TILES_AVAILABLE.add(TILE_PIE);
 	STATIC_TILES_AVAILABLE.add(TILE_APPCIRCLEBAR);
 	STATIC_TILES_AVAILABLE.add(TILE_APPSIDEBAR);
+	STATIC_TILES_AVAILABLE.add(TILE_RESTARTUI);
 	STATIC_TILES_AVAILABLE.add(TILE_KERNEL);
 	STATIC_TILES_AVAILABLE.add(TILE_SCREENRECORD);
 
