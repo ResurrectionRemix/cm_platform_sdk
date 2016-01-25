c9f8ebe7d38fd2b548affea2044d0d74b786de9f
Varun <date.varun123@gmail.com>
2016-01-25 12:34:22 +0530

    Add Power Menu Tile [2/2]
    Signed-off-by: Varun <date.varun123@gmail.com>

--- src/java/org/cyanogenmod/internal/util/QSConstants.java
+++ src/java/org/cyanogenmod/internal/util/QSConstants.java
@@ -67,7 +67,8 @@ public class QSConstants {
     public static final String TILE_APPCIRCLEBAR = "appcirclebar";
     public static final String TILE_APPSIDEBAR = "appsidebar";	
     public static final String TILE_RESTARTUI = "restartui";
-public static final String TILE_GESTUREANYWHERE = "gesture_anywhere";					
+    public static final String TILE_GESTUREANYWHERE = "gesture_anywhere";
+    public static final String TILE_POWER = "power_menu";						
     public static final String TILE_KERNEL= "kernel_adiutor";	
     public static final String TILE_SCREENRECORD= "screenrecord";	
     public static final String DYNAMIC_TILE_NEXT_ALARM = "next_alarm";
@@ -126,6 +127,7 @@ public static final String TILE_GESTUREANYWHERE = "gesture_anywhere";
 	STATIC_TILES_AVAILABLE.add(TILE_APPCIRCLEBAR);
 	STATIC_TILES_AVAILABLE.add(TILE_APPSIDEBAR);
 	STATIC_TILES_AVAILABLE.add(TILE_RESTARTUI);
+	STATIC_TILES_AVAILABLE.add(TILE_POWER);
 	STATIC_TILES_AVAILABLE.add(TILE_KERNEL);
 	STATIC_TILES_AVAILABLE.add(TILE_SCREENRECORD);
 	STATIC_TILES_AVAILABLE.add(TILE_GESTUREANYWHERE);
