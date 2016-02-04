cd95960a51cdcdac92d8b234723dce5126c65d8f
Keith Mok <kmok@cyngn.com>
2016-02-04 10:49:03 -0800

    CMSDK : Add caffeine tile (2/2)
    Change-Id: I6c3d5cdc0dc2cce06f1c00c6ffbb96d5f376dc46

--- src/java/org/cyanogenmod/internal/util/QSConstants.java
+++ src/java/org/cyanogenmod/internal/util/QSConstants.java
@@ -52,6 +52,7 @@ public class QSConstants {
     public static final String TILE_AMBIENT_DISPLAY = "ambient_display";
     public static final String TILE_SYNC = "sync";
     public static final String TILE_BATTERY_SAVER = "battery_saver";
+    public static final String TILE_CAFFEINE = "caffeine";
     public static final String TILE_EDIT = "edit";
     public static final String TILE_DND = "dnd";
     public static final String TILE_BRIGHTNESS = "brightness";
@@ -138,6 +139,7 @@ public class QSConstants {
 	STATIC_TILES_AVAILABLE.add(TILE_SCREENRECORD);
 	STATIC_TILES_AVAILABLE.add(TILE_GESTUREANYWHERE);
         STATIC_TILES_AVAILABLE.add(TILE_BATTERY_SAVER);
+        STATIC_TILES_AVAILABLE.add(TILE_CAFFEINE);
 
         TILES_AVAILABLE.addAll(STATIC_TILES_AVAILABLE);
 
