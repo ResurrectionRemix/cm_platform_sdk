ae095f560c077876221374974507203502caea8e
westcripp <korukaltan@gmail.com>
2016-03-11 18:24:01 +0200

    Revert "CMSettings: HeadsUp whitelists (1/3)"
    This reverts commit 7c23fcc666426c7970d599f87fa5cad25807fc4b.
    
    Conflicts:
    	src/java/cyanogenmod/providers/CMSettings.java

--- src/java/cyanogenmod/providers/CMSettings.java
+++ src/java/cyanogenmod/providers/CMSettings.java
@@ -1686,16 +1686,6 @@ public final class CMSettings {
         /** @hide */
         public static final Validator HEADS_UP_BLACKLIST_VALUES_VALIDATOR = sAlwaysTrueValidator;
 
-        /**
-         * Which applications to post all notification to heads up
-         *
-         * @hide
-         */
-        public static final String HEADS_UP_WHITELIST_VALUES = "heads_up_whitelist_values";
-
-        /** @hide */
-        public static final Validator HEADS_UP_WHITELIST_VALUES_VALIDATOR = sAlwaysTrueValidator;
-
         /** @hide */
         public static final Validator NOTIFICATION_LIGHT_PULSE_CUSTOM_VALUES_VALIDATOR =
                 new Validator() {
@@ -2038,7 +2028,6 @@ public final class CMSettings {
                     NOTIFICATION_LIGHT_PULSE_CUSTOM_VALUES_VALIDATOR);
             VALIDATORS.put(HEADS_UP_CUSTOM_VALUES, HEADS_UP_CUSTOM_VALUES_VALIDATOR);
             VALIDATORS.put(HEADS_UP_BLACKLIST_VALUES, HEADS_UP_BLACKLIST_VALUES_VALIDATOR);
-            VALIDATORS.put(HEADS_UP_WHITELIST_VALUES, HEADS_UP_WHITELIST_VALUES_VALIDATOR);
             VALIDATORS.put(NOTIFICATION_LIGHT_COLOR_AUTO,
                     NOTIFICATION_LIGHT_COLOR_AUTO_VALIDATOR);
             VALIDATORS.put(ZEN_ALLOW_LIGHTS, ZEN_ALLOW_LIGHTS_VALIDATOR);
