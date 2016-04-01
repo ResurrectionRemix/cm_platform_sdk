7bfbdf53dd062734af1490262cfa4d88eecbcb56
Adnan Begovic <adnan@cyngn.com>
2016-04-01 15:39:53 -0700

    cmsdk: Introduce Fig, api level 6.
    Change-Id: I2916b1462c1f0b9582f4fefac82f60c20f820b85
    TICKET: CYNGNOS-2303

--- sdk/src/java/cyanogenmod/os/Build.java
+++ sdk/src/java/cyanogenmod/os/Build.java
@@ -53,6 +53,7 @@ public class Build {
         sdkMap.put(CM_VERSION_CODES.CANTALOUPE, "Cantaloupe");
         sdkMap.put(CM_VERSION_CODES.DRAGON_FRUIT, "Dragon Fruit");
         sdkMap.put(CM_VERSION_CODES.ELDERBERRY, "Elderberry");
+        sdkMap.put(CM_VERSION_CODES.FIG, "Fig");
     }
 
     /** Various version strings. */
@@ -159,6 +160,11 @@ public class Build {
          * </ul>
          */
         public static final int ELDERBERRY = 5;
+
+        /**
+         * Future Release - 3rd quarter 2016
+         */
+        public static final int FIG = 6;
     }
 
     /**
